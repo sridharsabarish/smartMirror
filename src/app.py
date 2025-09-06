e4575c5495d14fab787b9aa31a81a0ccab2affa5
sabarish sridhar <sabarishsridhar@sabarishs-Mac-mini.local>
2025-09-06 12:10:29 +0200

    Some changes to make it run

--- src/app.py
+++ src/app.py
@@ -10,4 +10,4 @@ def serve_page():
     return render_template_string(html)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
